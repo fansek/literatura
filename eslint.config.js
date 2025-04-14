6581bb4c58c986b24842be087cf22cd30a61466a
fansek <fgrega@gmail.com>
2025-04-14 07:43:58 +0200

    Consolidate dependencies and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,10 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import typescriptEslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...typescriptEslint.configs.recommended,
-  eslintPluginPrettier,
-  {
-    rules: {
-      'prettier/prettier': [
-        'error',
-        {
-          singleQuote: true,
-        },
-      ],
-    },
-  },
 ];
