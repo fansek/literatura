197b7212594f03a557beda227d48ee901f70587a
fansek <fgrega@gmail.com>
2025-11-14 19:28:01 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export { default as build } from './cli/build.js';
-export { default as render } from './cli/render.js';
+export { default as buildWithTs } from './build/build-with-ts.js';
+export { default as render } from './render/render.js';
